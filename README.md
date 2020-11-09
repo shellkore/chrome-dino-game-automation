48c87781dac601a850b5882fb9e93bcae73484c8
shellkore <shellkoresahu@gmail.com>
2020-11-09 20:01:50 +0530

    cleaned code and added reqs

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # chrome-dino-game-automation
+
+## Description
 The dino will jump and crouch automatically. Yayy!!!
+Libraries used are PIL, pyautogui and pyscreenshot
+
+## Requirements
+
+`pip install -r requirements.txt`
+
+## Run
+
+- open `chrome://dino` in your browser
+- Run main.py. `python main.py` in terminal
+- Switch to your dino game's tab
--- main.py
+++ main.py
@@ -4,58 +4,47 @@ import pyscreenshot as ImageGrab
 import time
 import numpy as np
 
-def isCactus(data):
-	for i in range(420,580):
-		for j in range(430,450):
-			if data[i,j]<100:
-				return True
-	return False
-
-def isBird(data):
-	for i in range(310,380):
-		for j in range(380,410):
-			if data[i,j]<100:
-				return True
-	return False
-
 def collisionCheck(data):
+	#for dragon
+	for i in range(340,400):
+		for j in range(370,400):
+			if data[i,j]<100:
+				pyautogui.press('down')
+				return
 	# cactus
-	for i in range(420,580):
-		for j in range(430,450):
+	for i in range(340,520):
+		for j in range(410,450):
 			if data[i,j]<100:
-				return ('up')
+				pyautogui.press('up')
+				return	
 
-	#bird
-	for i in range(360,400):
-		for j in range(380,410):
-			if data[i,j]<100:
-				return('down')
-
-	return False
+	return
 
 def rectangleDraw():
 	time.sleep(3)
+	# for dragon
+	for i in range(340,400):
+		for j in range(370,400):
+			data[i,j] = 100
+	#for cactus
 	im = ImageGrab.grab().convert('L')
 	data = im.load()
-	for i in range(310,380):
-		for j in range(380,410):
+	for i in range(340,520):
+		for j in range(410,450):
 			data[i,j] = 0
+
+
 	im.show()
 
 def play():
+	time.sleep(4)
 	print("pressing up key.......")
 	pyautogui.press('up')
-	time.sleep(3)
+	time.sleep(2)
 	while True:
 		im = ImageGrab.grab().convert('L')
 		data = im.load()
-		keyToPress = (collisionCheck(data))
-		if keyToPress != False:
-			pyautogui.press(keyToPress)
-		# if isCactus(data):
-		# 	pyautogui.press('up')
-		# if isBird(data):
-		# 	pyautogui.press('down')
+		collisionCheck(data)
 
 if __name__ == '__main__':
 	play()	
