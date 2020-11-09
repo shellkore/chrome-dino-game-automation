3c09129faf3848fefb3b9efca1e4bd6d7df9e0f7
shellkore <shellkoresahu@gmail.com>
2020-11-09 14:41:08 +0530

    tried to improve accuracy

--- main.py
+++ main.py
@@ -11,20 +11,34 @@ def isCactus(data):
 				return True
 	return False
 
-
 def isBird(data):
-	for i in range(360,400):
-		for j in range(370,400):
+	for i in range(310,380):
+		for j in range(380,410):
 			if data[i,j]<100:
 				return True
 	return False
 
-def rectangelTest():
+def collisionCheck(data):
+	# cactus
+	for i in range(420,580):
+		for j in range(430,450):
+			if data[i,j]<100:
+				return ('up')
+
+	#bird
+	for i in range(360,400):
+		for j in range(380,410):
+			if data[i,j]<100:
+				return('down')
+
+	return False
+
+def rectangleDraw():
 	time.sleep(3)
 	im = ImageGrab.grab().convert('L')
 	data = im.load()
-	for i in range(560,440,-1):
-		for j in range(430,450):
+	for i in range(310,380):
+		for j in range(380,410):
 			data[i,j] = 0
 	im.show()
 
@@ -35,12 +49,15 @@ def play():
 	while True:
 		im = ImageGrab.grab().convert('L')
 		data = im.load()
-		if isCactus(data):
-			pyautogui.press('up')
-		if isBird(data):
-			pyautogui.press('down')
+		keyToPress = (collisionCheck(data))
+		if keyToPress != False:
+			pyautogui.press(keyToPress)
+		# if isCactus(data):
+		# 	pyautogui.press('up')
+		# if isBird(data):
+		# 	pyautogui.press('down')
 
 if __name__ == '__main__':
 	play()	
-	# rectangelTest()
+	# rectangleDraw()
 	
